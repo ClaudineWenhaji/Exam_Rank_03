d727a7be47265c1eccb831d5e33022e5cd1a1525
Claudine Wenhaji Ndomeni <clwenhaj@f1br5s1.clusters.42paris.fr>
2026-02-25 20:05:36 +0100

    n

--- Mine/get_next_line.c
+++ Mine/get_next_line.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clwenhaj <clwenhaj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clwenhaj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/18 16:03:56 by clwenhaj          #+#    #+#             */
-/*   Updated: 2026/02/25 14:43:12 by clwenhaj         ###   ########.fr       */
+/*   Created: 2026/02/25 16:18:01 by clwenhaj          #+#    #+#             */
+/*   Updated: 2026/02/25 16:39:55 by clwenhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "gnl.h"
+#include "get_next_line.h"
 
 char	*ft_strdup(char *str)
 {
+	char	*dup;
 	int	i = 0;
 	int	len = 0;
-	char	*dup;
 
 	while (str[len])
 		len++;
@@ -44,38 +44,22 @@ char	*get_next_line(int fd)
 		return (NULL);
 	while (1)
 	{
-		if (buffer_i >= buffer_size)	// tout ce quon avait lu a ete consomme
+		if (buffer_i >= buffer_size)
 		{
-			buffer_i = 0;				// on remet a 0 pour relire depuis le debut du fichier
 			buffer_size = read(fd, buffer, BUFFER_SIZE);
-			if (buffer_size <= 0)		// on quitte la boucle si read echoue (-1) ou read revoie 0 (fin du fichier)
+			buffer_i = 0;
+			if (buffer_size <= 0)
 				break ;
 		}
-		line[i] = buffer[buffer_i]; 	// copie caractere / caractere du buffer dans line
+		line[i] = buffer[buffer_i];
 		i++;
 		buffer_i++;
-		if (line[i - 1] == '\n')		// on arrete des quon rencontre \n
+		if (line[i - 1] == '\n')
 			break ;
 	}
-	if (i == 0)							// si aucun caractere lu i.e fin du fichier
+	if (i == 0)
 		return (NULL);
 	line[i] = '\0';
-	return (ft_strdup(line));			// renvoie une copie dynamique de la ligne
-}
-
-int	main(int ac, char **av)
-{
-	char	*line;
-	int	fd = 0;
-
-	fd = open(av[1], O_RDONLY);
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("Line %s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
+	return (ft_strdup(line));
+	
 }
---
+++ Mine/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clwenhaj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 16:14:44 by clwenhaj          #+#    #+#             */
+/*   Updated: 2026/02/25 16:17:32 by clwenhaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
+
+char	*ft_strdup(char *str);
+char	*get_next_line(int fd);
+
+#endif
